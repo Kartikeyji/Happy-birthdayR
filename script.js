804f912d8c080e3fedac2190f6e5d2bb1e9828e6
Kartikeyji <155772291+Kartikeyji@users.noreply.github.com>
2024-01-06 01:11:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add this function to play the audio
+function playAudio() {
+    var audio = document.getElementById("background-music");
+    audio.play();
+}
+
+// Your existing JavaScript code goes here
+
+// ...
+
